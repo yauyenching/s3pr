6c929256e1b886755cdf1e76500a4e8b52a13523
Yau Yen Ching <yauyenching@pm.me>
2022-09-15 00:34:14 +0800

    Update ProgramGUI
    - Change category grid for row 2 to 4 columns
    
    - Disable category_menu if change_category is unchecked
    
    - Change ComboBox to OptionMenu so that typing is disallowed

--- ProgramGUI.py
+++ ProgramGUI.py
@@ -103,6 +103,12 @@ class App(ctk.CTk):
                     self.status_update.set(
                         f"Completed extracting ICONs from {completed_files} files in {time_taken:.2f} seconds.")
 
+        def change_category_callback():
+            if self.change_category.get():
+                self.category_menu.configure(state=tk.NORMAL)
+            else:
+                self.category_menu.configure(state=tk.DISABLED)
+
         def extract_icon_callback():
             if self.extract_icon.get():
                 self.overwrite_checkbox.configure(state=tk.NORMAL)
@@ -173,7 +179,7 @@ class App(ctk.CTk):
 
         self.path_btn = ctk.CTkButton(
             master=self.row_1,
-            text="Browse",
+            text="Browse", text_color="white",
             image=self.open_icon, command=get_folder_path_callback, width=100)
         self.path_btn.grid(row=1, column=1, sticky='EW', padx=(0, 15), pady=10)
 
@@ -181,48 +187,51 @@ class App(ctk.CTk):
 
         self.row_2.rowconfigure(0, weight=1)
         self.row_2.rowconfigure(1, weight=1)
-        self.row_2.columnconfigure(0, weight=1)
-        self.row_2.columnconfigure(9, weight=1)
+        self.row_2.columnconfigure((0, 1, 2), weight=3)
+        self.row_2.columnconfigure(3, weight=1)
 
         self.tool_label = ctk.CTkLabel(self.row_2, text=self.TOOL_LABEL)
-        self.tool_label.grid(row=0, column=0, sticky=tk.N, columnspan=10)
+        self.tool_label.grid(row=0, column=0, sticky=tk.N, columnspan=4)
 
         self.progress_bar = ctk.CTkProgressBar(self.row_2, height=15)
         self.progress_bar.grid(row=1, column=0, padx=(
-            15, 10), sticky='we', columnspan=9, pady=(10, 0))
+            15, 10), sticky='we', columnspan=3, pady=(10, 0))
         self.progress_bar.set(0)
 
         # Creating a photoimage object to use image
         self.run_icon = tk.PhotoImage(file=r"assets/energy.png")
 
-        self.run_btn = ctk.CTkButton(self.row_2, text="Run",
+        self.run_btn = ctk.CTkButton(self.row_2, text="Run", text_color="white",
                                      image=self.run_icon, command=run_program)
-        self.run_btn.grid(row=1, column=9, sticky=tk.E,
+        self.run_btn.grid(row=1, column=3, sticky=tk.E,
                           padx=(0, 15), pady=(10, 0))
 
-        self.category_menu = ctk.CTkComboBox(
-            self.row_2, values=self.CATEGORIES, border_width=1)
+        self.category_menu = ctk.CTkOptionMenu(
+            self.row_2, values=self.CATEGORIES, 
+            fg_color=("white", "gray24"), button_color=("gray70", "gray32"),
+            button_hover_color=("#6E7174", "#7A848D"))
         self.category_menu.grid(
             row=2, column=0, sticky='we', padx=(15, 10), pady=10)
 
         self.change_category_checkbox = ctk.CTkCheckBox(
             self.row_2, text="Change category", 
             border_width=1.5, width=16, height=16, corner_radius=5, pady=10, 
-            variable=self.change_category, onvalue=True, offvalue=False)
-        self.change_category_checkbox.grid(row=2, column=7, padx=(0, 30))
+            variable=self.change_category, onvalue=True, offvalue=False,
+            command=change_category_callback)
+        self.change_category_checkbox.grid(row=2, column=1, padx=(0, 20))
 
         self.extract_icon_checkbox = ctk.CTkCheckBox(
-            self.row_2, text="Extract icon", 
+            self.row_2, text="Extract icon",
             border_width=1.5, width=16, height=16, corner_radius=5, pady=10, 
             variable=self.extract_icon, onvalue=True, offvalue=False, 
             command=extract_icon_callback)
-        self.extract_icon_checkbox.grid(row=2, column=8)
+        self.extract_icon_checkbox.grid(row=2, column=2)
 
         self.overwrite_checkbox = ctk.CTkCheckBox(
             self.row_2, text="Overwrite icon with\nsame filename", 
             border_width=1.5, width=16, height=16, corner_radius=5, pady=10, 
             variable=self.overwrite, onvalue=True, offvalue=False)
-        self.overwrite_checkbox.grid(row=2, column=9)
+        self.overwrite_checkbox.grid(row=2, column=3)
 
         # ============ row_3 ============
 
